99a1b40e40ac612539bbdd34e7242a08a3580af9
sanjayb-1208 <sanjayb10122005@gmail.com>
2026-02-06 23:34:41 +0530

    Update Signup.jsx

--- frontend/notes/src/pages/Signup.jsx
+++ frontend/notes/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { AppContent } from '../context/AppContext';
+import { AppContext } from '../context/AppContext';
 
 const Signup = () => {
     const {backendUrl} = useContext(AppContext);
@@ -99,6 +99,7 @@ const Signup = () => {
 
 
 export default Signup;
+
 
 
 
